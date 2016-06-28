8deda0ea822cba1ccf1986f7dde329cef20cb11d
jackie0025 <jackie0025@163.ccom>
2016-06-28 11:37:25 +0800

    add dbhelper class, and tables

---
+++ app/src/main/java/com/ceair/www/weather/model/City.java
@@ -0,0 +1,43 @@
+package com.ceair.www.weather.model;
+
+/**
+ * Created by JZ on 2016/6/28.
+ */
+public class City {
+    private int id;
+    private String cityName;
+    private String cityCode;
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public String getCityCode() {
+        return cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public void setCityCode(String cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
